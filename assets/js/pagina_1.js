828ce5edcfd6b0dd85e8a2e47f2071920b8ee91f
tomasmolinaarias <tomasmolinaarias12@gmail.com>
2021-10-01 19:54:03 -0300

    agregue modal al boton de las tarjetas

--- assets/js/pagina_1.js
+++ assets/js/pagina_1.js
@@ -51,11 +51,12 @@ $(document).ready(function(){
       $(".tarjeta4").dblclick(function(){
           $(this).css("background-color","turquoise");
       });
-        //popover
-      var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-      var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-      return new bootstrap.Popover(popoverTriggerEl)
-      })
+        //modal
+        var myModal = document.getElementById('myModal')
+        var myInput = document.getElementById('myInput')        
+        myModal.addEventListener('shown.bs.modal', function () {
+          myInput.focus()
+        })
 
       //tooltip
       var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
