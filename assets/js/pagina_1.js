bbe65be0e6e80bbf6bcaaa59d94d57d13682052e
tomasmolinaarias <tomasmolinaarias12@gmail.com>
2021-10-02 23:47:35 -0300

    ordene js

--- assets/js/pagina_1.js
+++ assets/js/pagina_1.js
@@ -1,75 +1,73 @@
 /*desaparecer carta*/
 
 $(document).ready(function(){
-       //desaparecer quienes somos
+//desaparecer quienes somos
     $(".quienes_somos").dblclick(function(){
       $(this).toggle("hide");
-       //cambio decolor  quienes somos 
     });
+//cambio decolor  quienes somos    
     $("#avion").click(function(){
-        $(this).css("background-color","red");
-        
-      });
-      $("#montaña").click(function(){
-        $(this).css("background-color","purple");
-      });
-      $("#ruta").click(function(){
-        $(this).css("background-color","pink");
-      });
-      $(".parrafo_QS1").click(function(){
-        $(this).css("color","red");
-      });
-      $(".parrafo_QS2").click(function(){
-        $(this).css("color","purple");
-      });
-      $(".parrafo_QS3").click(function(){
-        $(this).css("color","pink");
-      });
-        //cambio de color tarjetas   
-      $(".tarjeta1").click(function(){
-        $(this).css("background-color","brown");
-        });
-        $(".tarjeta2").click(function(){
-          $(this).css("background-color","pink");
-      });
-      $(".tarjeta3").click(function(){
-          $(this).css("background-color","orange");
-      });
-      $(".tarjeta4").click(function(){
-          $(this).css("background-color","red");
-      });
-        //con doble click vuelve ala normalida
-      $(".tarjeta1").dblclick(function(){
-          $(this).css("background-color","turquoise");
-      });
-      $(".tarjeta2").dblclick(function(){
-          $(this).css("background-color","turquoise");
-      });
-      $(".tarjeta3").dblclick(function(){
-          $(this).css("background-color","turquoise");
-      });
-      $(".tarjeta4").dblclick(function(){
-          $(this).css("background-color","turquoise");
-      });
-      //alert footer cuando haces click en el viajes chile
-      $(".footer_titulo").click(function(){
-        alert ("se cambiara a la misma pagina pero en githubpage")
-      });
-      //alert navs cuando haga click en viajes chile
-      $(".menu_titulo").click(function(){
-        alert ("se cambiara a la misma pagina pero en netlify")
-      });
-      
-        //modal
-        var myModal = document.getElementById('myModal')
-        var myInput = document.getElementById('myInput')        
-        myModal.addEventListener('shown.bs.modal', function () {
-          myInput.focus()
-        })
+      $(this).css("background-color","red");    
+    });
+    $("#montaña").click(function(){
+      $(this).css("background-color","purple");
+    });
+    $("#ruta").click(function(){
+      $(this).css("background-color","pink");
+    });
+    $(".parrafo_QS1").click(function(){
+      $(this).css("color","red");
+    });
+    $(".parrafo_QS2").click(function(){
+      $(this).css("color","purple");
+    });
+    $(".parrafo_QS3").click(function(){
+      $(this).css("color","pink");
+    });
+//cambio de color tarjetas   
+    $(".tarjeta1").click(function(){
+      $(this).css("background-color","brown");
+    });
+    $(".tarjeta2").click(function(){
+      $(this).css("background-color","pink");
+    });
+    $(".tarjeta3").click(function(){
+      $(this).css("background-color","orange");
+    });
+    $(".tarjeta4").click(function(){
+      $(this).css("background-color","red");
+    });
+//con doble click vuelve ala normalida
+    $(".tarjeta1").dblclick(function(){
+      $(this).css("background-color","turquoise");
+    });
+    $(".tarjeta2").dblclick(function(){
+      $(this).css("background-color","turquoise");
+    });
+    $(".tarjeta3").dblclick(function(){
+      $(this).css("background-color","turquoise");
+    });
+    $(".tarjeta4").dblclick(function(){
+      $(this).css("background-color","turquoise");
+    });
+//alert footer cuando haces click en el viajes chile
+    $(".footer_titulo").click(function(){
+      alert ("se cambiara a la misma pagina pero en githubpage")
+    });
+//alert navs cuando haga click en viajes chile
+    $(".menu_titulo").click(function(){
+      alert ("se cambiara a la misma pagina pero en netlify")
+    });      
+//modal
+    var myModal = document.getElementById('myModal')
+    var myInput = document.getElementById('myInput')        
+    myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+    })
 
-      //tooltip
-      var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-      var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-      })
+//tooltip
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
 });
